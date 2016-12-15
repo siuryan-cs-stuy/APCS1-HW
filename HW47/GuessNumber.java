cd224216a87c1d9ef476df9db3e7d5f886608c07
Ryan Siu <rsiu@stuy.edu>
2016-12-15 15:01:40 -0500

    HW47

--- HW47/GuessNumber.java
+++ HW47/GuessNumber.java
@@ -1,11 +1,19 @@
+// Ryan Siu
+// APCS1 pd5
+// HW47 -- Keep Guessing
+// 2016-12-15
+
 import cs1.Keyboard;
 
 public class GuessNumber {
 
+    // instance vars
     private int randNum;
     private int high;
     private int low;
 
+    
+    // default constructor
     public GuessNumber() {
 	randNum = (int)(Math.random()*100) + 1;
 	high = 100;
@@ -15,14 +23,23 @@ public class GuessNumber {
     // Returns positive if too high, negative if too low, 0 if correct
     public int checkGuess( int guess ) {
 	int diff = guess - randNum;
-	if (diff < 0 && guess > low) {
-	    low = guess;
+	if (diff < 0) {
+	    if (guess > low) {
+		low = guess;
+	    } else {
+		throw new UnsupportedOperationException( "Input guess for checkGuess(int) was out of range" );
+	    }
 	} else {
-	    high = guess;
+	    if (guess < high) {
+		high = guess;
+	    } else {
+		throw new UnsupportedOperationException( "Input guess for checkGuess(int) was out of range" );
+	    }
 	}
 	return diff;
     }
 
+    // gameplay
     public static void main( String[] args ) {
 
 	GuessNumber g = new GuessNumber();
@@ -30,16 +47,21 @@ public class GuessNumber {
 	int guesses = 0;
 
 	while (!win) {
-	    System.out.print( "Guess a number between " + g.low + " and " + g.high + ": " );
+	    System.out.print( "Guess a number from " + g.low + "-" + g.high + ": " );
 	    int input = Keyboard.readInt();
-	    if (g.checkGuess(input) > 0) {
-		System.out.println( "\nToo high, try again..." );
-	    } else if (g.checkGuess(input) < 0) {
-		System.out.println( "\nToo low, try again..." );
-	    } else {
-		win = true;
+	    try {
+		int result = g.checkGuess(input);
+		if (result > 0) {
+		    System.out.println( "Too high, try again..." );
+		} else if (result < 0) {
+		    System.out.println( "Too low, try again..." );
+		} else if (result == 0){
+		    win = true;
+		}
+		guesses++;
+	    } catch (UnsupportedOperationException e) {
+		System.out.println( "Your guess was out of the given range..." );
 	    }
-	    guesses++;
 	}
 
 	System.out.println( "Correct! It took " + guesses + " guesses." );
