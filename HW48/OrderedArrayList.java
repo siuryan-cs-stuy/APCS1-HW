f217cd61d5c18d21d3a7e575d94c66436bdb758f
Ryan Siu <rsiu@stuy.edu>
2016-12-19 17:31:13 -0500

    HW48

--- HW48/OrderedArrayList.java
+++ HW48/OrderedArrayList.java
@@ -72,24 +72,44 @@ public class OrderedArrayList {
     // maintains ascending order of elements
     // uses a binary search to find appropriate index
     public void addBinary( Comparable newVal ) {
+
+	// Prevent error when getting value at mid
+	if ( _data.size() == 0 ) {
+	    _data.add(newVal);
+	    return;
+	}
+	
         int low = 0;
-	int high = _data.size() - 1;
-	int mid;
-	while (low != high) {
+	int high = _data.size()-1; // to get the highest index
+	int mid = 0;
+
+	// Binary search section
+	while (high >= low) {
 
 	    mid = (low + high) / 2;
-	    int midValue = _data.get(mid);
 
 	    // Uses the Comparable method compareTo (if newVal is less than
-	    // get(i), then a negative integer is returned
-	    if (newVal.compareTo( midValue ) < 0) {
-		high = midValue;
-	    } else if (newVal.compareTo( midValue ) > 0) {
-		low = midValue;
+	    // the value at mid, then a negative integer is returned
+	    if (newVal.compareTo( _data.get(mid) ) < 0) {
+	        high = mid - 1;
+	    } else if (newVal.compareTo( _data.get(mid) ) > 0) {
+		low = mid + 1;
 	    } else {
-		_data.add(mid, newVal);
+	        break;
 	    }
+	    
 	}
+
+	// Add section
+	// Compares the value at index mid to newVal
+	// If newVal is greater, insert at index mid + 1
+	// Otherwise if equal or less, insert at index mid
+	if (newVal.compareTo( _data.get(mid) ) > 0) {
+	    _data.add(mid+1, newVal);
+	} else {
+	    _data.add(mid, newVal);
+	}
+	
 	
     }
 
@@ -99,16 +119,16 @@ public class OrderedArrayList {
 
 	OrderedArrayList Franz = new OrderedArrayList();
 
-	System.out.println("\nValues to add via addLinear() calls:");
+	System.out.println("\nValues to add via addBinary() calls:");
 
 	// testing linear search
 	for( int i = 0; i < 15; i++ ) {
 	    int valToAdd = (int)( 50 * Math.random() );
 	    System.out.println( valToAdd );
-	    Franz.addLinear( valToAdd );
+	    Franz.addBinary( valToAdd );
 	}
 
-	System.out.println("\nafter population via addLinear() calls:");
+	System.out.println("\nafter population via addBinary() calls:");
 	System.out.println( Franz );
 	System.out.println();
 	/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
